08a6265ec7674e05e3ce004614571b44485cfc15
Yuanjie Li <yuanjie.li@utexas.edu>
2024-03-19 23:17:38 -0600

    [ADD] checks for temp / V throttle

--- shell/record.sh
+++ shell/record.sh
@@ -17,9 +17,13 @@ else
 		echo "New FileName: " "$new_fileName"
 		
 		#This was the toughest part of the whole project to find the optimal settings for recording.
-		libcamera-vid -t 60000 --width 720 --height 480 --framerate 10 -b 9000000 --codec libav -o  $new_fileName
+		libcamera-vid -t 60000 --width 1920 --height 1080 --framerate 10 -b 1024k --codec libav -o  $new_fileName
 
 		echo "Done with recording..."
+
+		# Check for throttling 
+		echo "Checking for throttle at cycle $i" >> throttle_logs.txt
+		vcgencmd get_throttled >> throttle_logs.txt
 
 	done
 fi
