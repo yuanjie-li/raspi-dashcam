736a313af750239470f5a021a4cae48c3d69d04b
Yuanjie Li <yuanjie.li@utexas.edu>
2024-03-20 22:42:52 -0600

    [FIX] framerate to 15

--- shell/test_record.sh
+++ shell/test_record.sh
@@ -2,19 +2,9 @@
  
 file_name=/home/hei/Videos/raspicam_
 
+echo "Testing recording ..."
 for i in `seq 1 1 1` 
 do
-
-	# Early kill if connects to home network
-	if [ $(iwgetid -r) = 'Winternet-5G' ]; 
-	then
-		echo
-		echo "ALERT: Connected to home network."
-		echo "No recording."
-		echo
-		break
-	fi
-
 	echo "Attempt $i times..."
 
 	current_time=$(date "+%b-%d-%Y__%H.%M.%S")
@@ -23,8 +13,7 @@ do
 	new_fileName=$file_name$current_time.mp4
 	echo "New FileName: " "$new_fileName"
 	
-	# This was the toughest part of the whole project to 
-	# find the optimal settings for recording.
+	#This was the toughest part of the whole project to find the optimal settings for recording.
 	libcamera-vid -t 60000 --width 1920 --height 1080 --framerate 15 -b 4096k --codec libav -o  $new_fileName
 
 	echo "Done with recording..."
